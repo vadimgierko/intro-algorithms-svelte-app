f4ef0a90201ddbaed638ac1402895d815ec4710c
vadimgierko <85487884+vadimgierko@users.noreply.github.com>
2023-09-15 21:12:24 +0200

    add random index pivot to quick sort and split components

---
+++ src/routes/quick-sort/StepsContainer.svelte
@@ -0,0 +1,24 @@
+<script>
+	import StepsList from './StepsList.svelte';
+
+	export let steps;
+	export let stepsForRandomIndexPivot;
+</script>
+
+<div id="container">
+	<div>
+		<p>Steps for <strong>not random pivot</strong>:</p>
+		<StepsList {steps} />
+	</div>
+
+	<div>
+		<p>Steps for <strong>random pivot</strong>:</p>
+		<StepsList steps={stepsForRandomIndexPivot} />
+	</div>
+</div>
+
+<style>
+	#container {
+		display: flex;
+	}
+</style>
